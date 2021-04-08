2d2e8ab83d5e9c38855830631db6981d1b78ff1c
Jorge A. Rodríguez Lopera <jarol72@gmail.com>
2021-04-08 18:51:24 -0500

    Completed gallery zoom viewer.

--- js/scripts.js
+++ js/scripts.js
@@ -14,14 +14,20 @@ window.onscroll = () => {
 
 
 /* ++++++++++ ZOOM VIEWER +++++++++++++++ */
-const zoomBtn = document.querySelectorAll('.zoom-text');
-const allImages = document.querySelectorAll('.img-container');
+const zoomBtn = document.querySelectorAll('.back');
+const allImages = document.querySelectorAll('.front');
+const imgNames = document.querySelectorAll('.image')
 const imageView = document.querySelector('.image-view');
 const nextBtn = document.getElementById('next-btn');
 const prevBtn = document.getElementById('prev-btn');
 const imageBox = document.querySelector('.image-box');
 
 let currentImageIdx = 0;
+let names = [];
+
+imgNames.forEach(function(img, index){
+    names[index] = img.getAttribute('src')
+})
 
 imageView.addEventListener('click', function(){
     this.style.display = "none";
@@ -30,30 +36,34 @@ imageView.addEventListener('click', function(){
 
 
 zoomBtn.forEach(function(btn, index){
+    btn.dataset.image = names[currentImageIdx];
     btn.addEventListener('click', function(){
         imageView.style.display = "block";
         imageBox.style.display = "block";
-        currentImageIdx = index + 1;
-        currentImageDisplay(currentImageIdx);
+        currentImageIdx = index;
+        currentImageDisplay(names[currentImageIdx]);
+        console.log(names[currentImageIdx]);
     })
 })
 
 function currentImageDisplay(position){
-    imageBox.style.background = `url(images/img${currentImageIdx}.jpg) center/cover no-repeat`;
+    console.log(position)
+    imageBox.style.background = `url("${position}") center/contain no-repeat`;
 }
 
 prevBtn.addEventListener('click', function(){
     currentImageIdx--;
-    if(currentImageIdx === 0){
-        currentImageIdx = allImages.length;
+    console.log(currentImageIdx);
+    if(currentImageIdx < 0){
+        currentImageIdx = names.length - 1;
     }
-    currentImageDisplay(currentImageIdx);
+    currentImageDisplay(names[currentImageIdx]);
 })
 
 nextBtn.addEventListener('click', function(){
     currentImageIdx++;
-    if(currentImageIdx === 5){
-        currentImageIdx = 1;
+    if(currentImageIdx === names.length){
+        currentImageIdx = 0;
     }
-    currentImageDisplay(currentImageIdx);
+    currentImageDisplay(names[currentImageIdx]);
 })
