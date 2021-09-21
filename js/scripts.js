047ef336b56c0f75ef0e6ddb016c8c3b7e611ab7
Jorge A. Rodríguez Lopera <jarol72@gmail.com>
2021-09-20 23:36:13 -0500

    Changed 'location.origin' to 'location.href' in line 63, script.js

--- js/scripts.js
+++ js/scripts.js
@@ -60,7 +60,7 @@ $(document).ready(function () {
   document.addEventListener("click", (e) => {
     if (e.target.matches(".card__overlay")) {
       sessionStorage.setItem("vblelocal", e.target.dataset.category);
-      location = `${location.origin}/gallery.html`;
+      location = `${location.href}/gallery.html`;
       console.log(`Session Storage: ` + sessionStorage.getItem("vblelocal"));
     }
   });
