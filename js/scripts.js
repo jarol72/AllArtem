6bbfdf0b7c6a776f3f6811050c359a5f84ee7565
Jorge Rodríguez Lopera <jarol72@gmail.com>
2022-09-25 17:09:01 -0500

    Fixed zomm viewer and navbar links

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,4 @@
-// +++++++++++++ ARTWORK GALLERY FILTER ++++++++++++++
-$(document).ready(function () {
-  $(".filter__option").click(function () {
-    const value = $(this).attr("data-filter");
-    if (value == "all") {
-      $(".artwork").show("1000");
-    } else {
-      $(".artwork")
-        .not("." + value)
-        .hide("1000");
-      $(".artwork")
-        .filter("." + value)
-        .show("1000");
-    }
-  });
 
-  // add active class on selected item
-  $(".filter__option").click(function () {
-    $(this)
-      .addClass("filter__option-active")
-      .siblings()
-      .removeClass("filter__option-active");
-  });
-
-  $(`[data-filter="${sessionStorage.getItem("vblelocal")}"]`).trigger(
-    "click"
-  );
-  /* } */
-});
 
 // +++++++++++++ MENÚ ANIMATION ++++++++++++++
 $(document).ready(function () {
@@ -78,7 +50,7 @@ document.addEventListener("click", (e) => {
       e.target.matches(".card__overlay__text") || 
       e.target.matches(".btn__products")) {
         let showCategory = e.target.hasAttribute('data-category')
-                              ? e.target.dataset.categori
+                              ? e.target.dataset.category
                               : e.target.closest('.card__overlay').dataset.category;
         console.log(showCategory);
         sessionStorage.setItem("vblelocal", showCategory);
@@ -89,73 +61,3 @@ document.addEventListener("click", (e) => {
   }
 });
 
-/* ++++++++++ ZOOM VIEWER +++++++++++++++ */
-$(document).ready(function () {
-  const zoomBtn = document.querySelectorAll(".back");;
-const allImages = document.querySelectorAll(".front");
-const imgNames = document.querySelectorAll(".image");
-const imageView = document.querySelector(".image-view");
-const nextBtn = document.getElementById("next-btn");
-const prevBtn = document.getElementById("prev-btn");
-const imageBox = document.querySelector(".image-box");
-
-let currentImageIdx = 0;
-let names = [];
-let selected = [];
-
-imgNames.forEach(function (img, index) {
-  names[index] = img.getAttribute("src");
-});
-
-if (imageView) {
-  imageView.addEventListener("click", function () {
-    this.style.display = "none";
-    imageBox.style.display = "none";
-    selected = [];
-  });
-}
-
-zoomBtn.forEach(function (btn, index) {
-  btn.dataset.image = names[index];
-  btn.addEventListener("click", function (e) {
-    if (document.querySelector('[data-filter="all"]').classList.contains('filter__option-active')) {
-      selected = names;
-    }
-
-    let category = btn.dataset.category;
-    zoomBtn.forEach((artwork) => {
-      if (artwork.dataset.category === category) {
-        selected.push(artwork.dataset.image);
-      }
-    });
-    imageView.style.display = "block";
-    imageBox.style.display = "block";
-    currentImageIdx = selected.indexOf(btn.dataset.image);
-    currentImageDisplay(selected[currentImageIdx]);
-  });
-});
-
-function currentImageDisplay(position) {
-  imageBox.style.background = `url("${position}") center/contain no-repeat`;
-}
-
-if (prevBtn) {
-  prevBtn.addEventListener("click", function () {
-    currentImageIdx--;
-    if (currentImageIdx < 0) {
-      currentImageIdx = selected.length - 1;
-    }
-    currentImageDisplay(selected[currentImageIdx]);
-  });
-}
-
-if (nextBtn) {
-  nextBtn.addEventListener("click", function () {
-    currentImageIdx++;
-    if (currentImageIdx === selected.length) {
-      currentImageIdx = 0;
-    }
-    currentImageDisplay(selected[currentImageIdx]);
-  });
-}
-});
