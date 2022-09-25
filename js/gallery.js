8f6c7ebd0cfa945229c72515764fd01def1091c4
Jorge Rodríguez Lopera <jarol72@gmail.com>
2022-09-25 17:09:01 -0500

    Fixed zomm viewer and navbar links

--- js/gallery.js
+++ js/gallery.js
@@ -350,3 +350,106 @@ $gallery.appendChild($fragment);
     </div>
 </article>
 */
+
+// +++++++++++++ ARTWORK GALLERY FILTER ++++++++++++++
+$(document).ready(function () {
+    $(".filter__option").click(function () {
+      const value = $(this).attr("data-filter");
+      if (value == "all") {
+        $(".artwork").show("1000");
+      } else {
+        $(".artwork")
+          .not("." + value)
+          .hide("1000");
+        $(".artwork")
+          .filter("." + value)
+          .show("1000");
+      }
+    });
+  
+    // add active class on selected item
+    $(".filter__option").click(function () {
+      $(this)
+        .addClass("filter__option-active")
+        .siblings()
+        .removeClass("filter__option-active");
+    });
+  
+    $(`[data-filter="${sessionStorage.getItem("vblelocal")}"]`).trigger(
+      "click"
+    );
+    /* } */
+  });
+
+/* ++++++++++ ZOOM VIEWER +++++++++++++++ */
+$(document).ready(function () {
+    const zoomBtn = document.querySelectorAll(".back");;
+    const allImages = document.querySelectorAll(".front");
+    const imgNames = document.querySelectorAll(".image");
+    const imageView = document.querySelector(".image-view");
+    const nextBtn = document.getElementById("next-btn");
+    const prevBtn = document.getElementById("prev-btn");
+    const imageBox = document.querySelector(".image-box");
+    
+    let currentImageIdx = 0;
+    let names = [];
+    let selected = [];
+    
+    imgNames.forEach(function (img, index) {
+      names[index] = img.getAttribute("src");
+    });
+    
+    if (imageView) {
+      imageView.addEventListener("click", function () {
+        this.style.display = "none";
+        imageBox.style.display = "none";
+        selected = [];
+      });
+    }
+    
+    zoomBtn.forEach(function (btn, index) {
+      btn.dataset.image = names[index];
+      btn.addEventListener("click", function (e) {
+        if (document.querySelector('[data-filter="all"]').classList.contains('filter__option-active')) {
+          selected = names;
+        }
+    
+        let category = btn.dataset.category;
+        zoomBtn.forEach((artwork) => {
+          if (artwork.dataset.category === category) {
+            selected.push(artwork.dataset.image);
+          }
+        });
+        imageView.style.display = "block";
+        imageBox.style.display = "block";
+        currentImageIdx = selected.indexOf(btn.dataset.image);
+        currentImageDisplay(selected[currentImageIdx]);
+      });
+    });
+    
+    function currentImageDisplay(position) {
+      imageBox.style.background = `url("${position}") center/contain no-repeat`;
+    }
+    
+    // View previous image
+    if (prevBtn) {
+      prevBtn.addEventListener("click", function () {
+        currentImageIdx--;
+        if (currentImageIdx < 0) {
+          currentImageIdx = selected.length - 1;
+        }
+        currentImageDisplay(selected[currentImageIdx]);
+      });
+    }
+    
+    // View next image
+    if (nextBtn) {
+      nextBtn.addEventListener("click", function () {
+        currentImageIdx++;
+        if (currentImageIdx === selected.length) {
+          currentImageIdx = 0;
+        }
+        currentImageDisplay(selected[currentImageIdx]);
+      });
+    }
+    });
