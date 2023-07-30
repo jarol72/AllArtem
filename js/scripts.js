9a66f002d3ca2480c843012532e46c1c00e29a54
Jorge A. Rodriguez Lopera <jarol72@gmail.com>
2023-07-30 11:27:36 -0500

    [ADD] Form validation

--- js/scripts.js
+++ js/scripts.js
@@ -45,6 +45,7 @@ window.onscroll = () => {
   }
 };
 
+// +++++++++++++ HANDLE CLICK ON CATEGORY (INDEX) ++++++++++++++
 document.addEventListener("click", (e) => {
   if (e.target.matches(".card__overlay") || 
       e.target.matches(".card__overlay__text") || 
@@ -61,3 +62,41 @@ document.addEventListener("click", (e) => {
   }
 });
 
+// +++++++++++++ VALIDATE FORM ++++++++++++++
+// JavaScript para validar el formulario
+function validateForm() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+  const dataAuthorization = document.getElementById("dataAutorization").checked;
+
+  // Validar que los campos obligatorios no estén vacíos
+  if (!name || !email || !message) {
+    alert("Por favor, complete todos los campos obligatorios.");
+    return false;
+  }
+
+  // Validar el formato del correo electrónico usando una expresión regular
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    alert("Por favor, ingrese un correo electrónico válido.");
+    return false;
+  }
+
+  // Validar que se haya marcado la casilla de autorización de tratamiento de datos personales
+  if (!dataAuthorization) {
+    alert("Por favor, autorice el tratamiento de sus datos personales.");
+    return false;
+  }
+
+  // Aquí puedes agregar más validaciones si es necesario, como validar el tamaño máximo de texto, etc.
+
+  // Si todo está bien, puedes enviar el formulario al servidor
+  // Aquí debes decidir si prefieres dejar que el formulario se envíe normalmente o si quieres enviarlo mediante JavaScript (usando AJAX, por ejemplo).
+  // Puedes descomentar la siguiente línea para enviar el formulario usando JavaScript:
+  // document.contact.submit();
+
+  // Si no deseas enviar el formulario automáticamente mediante JavaScript,
+  // entonces asegúrate de devolver true para que el envío continúe normalmente.
+  return true;
+}
